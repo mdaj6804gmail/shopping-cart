691b63ac5258b6432a4cf99e628f11bfe8c4c858
MD All Mubin <mdaj6804@gmail.com>
2023-01-21 12:27:10 +0600

    mid

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const plus_icon = document.getElementById("plus_icon");
-const minus_icon = document.getElementById("minus_icon");
-const item_valu= document.getElementById("item_valu");
-let m=1;
-plus_icon.addEventListener("click", function () {
+//phone 
+prodectQuantity("phone-plus", "phone-cuntiti", +1, "phone-price", 1219);
+prodectQuantity("phone-minus", "phone-cuntiti", -1, "phone-price", 1219);
 
-  m++;
-   m = (a < 10) ;
-   item_valu.innerText=m;
-});
-
-// const plus = document.querySelector(".plus");
-//    const minus = document.querySelector(".minus");
-//    const num = document.querySelector(".num");
-//     let a = 1;
-//     plus.addEventListener("click", ()=>{
-//       a++;
-//       a = (a < 10) ? "0" + a : a;
-//       num.innerText = a;
-//     });
-
-//     minus.addEventListener("click", ()=>{
-//       if(a > 1){
-//         a--;
-//         a = (a < 10) ? "0" + a : a;
-//         num.innerText = a;
-//       }
-//     });
+//case
+prodectQuantity("case-plus", "case-quantity", 1, "case-total", 59);
+prodectQuantity("case-minus", "case-quantity", -1, "case-total", 59);
 
 
 
@@ -36,38 +15,23 @@ plus_icon.addEventListener("click", function () {
 
 
 
+function prodectQuantity(id, count, val, priceCount, priceMain) {
+
+    document.getElementById(id).addEventListener("click", () => {
+        let phoneCuntiti = document.getElementById(count);
+        let phoneCount = parseInt(phoneCuntiti.value);
+        let NewCount = phoneCount + val;
+        phoneCuntiti.value = NewCount;
+        //price
+        let Price = NewCount * priceMain;
+        document.getElementById(priceCount).innerText = Price;
+        // document.getElementById("sub-total").innerText =1278+priceMain;
+
+        // console.log(price)
+    })
+}
 
 
 
 
 
-
-
-
-// function plus_muns_V (id){
-//     m++;
-//   document.getElementById(id).value=m;
-
-// }
-
-
-  
-
-
-// <script>
-
-// var n = 0;
-
-// function buttonClick() {
-
-// n++;
-
-// document.getElementById('increament').value = n;
-
-// }
-
-// </script>
-
-// <button onclick="buttonClick();">Click Me</button>
-
-// <input type="text" id="increament" value="0"></input>
